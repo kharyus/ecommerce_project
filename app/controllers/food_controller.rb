c2452a7e84a88a459c1fe58cbca9b818242d0894
Ubuntu <vagrant@Base-Box>
2018-11-04 16:26:54 +0000

    added routes.

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,7 +1,9 @@
 class FoodController < ApplicationController
   def index
+
   end
 
   def show
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'food/index'
-  get 'food/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :foods, only: [:index, :show]
+  
+  root to: 'foods#index'
 end
